a7767627575b868ca4911039cddd473319345bfd
Adam Cross <tw2plus@outlook.com>
2017-11-05 19:03:58 +0000

    created first grain, working through web api

---
+++ GrepoApi/Controllers/PingPongController.cs
@@ -0,0 +1,41 @@
+﻿using GrepoTools.Distributed.GrainInterfaces;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GrepoApi.Controllers
+{
+    /// <summary>
+    /// A test controller for testing the connection is up and running.
+    /// </summary>
+    [RoutePrefix("api/ping")]
+    public class PingPongController : ApiController
+    {
+        public PingPongController(IClusterClient client)
+        {
+            Client = client;
+        }
+
+        private IClusterClient Client { get; }
+
+        /// <summary>
+        /// Connects to Orleans and returns some text.
+        /// </summary>
+        /// <param name="text">Input Text</param>
+        /// <returns>Output Text</returns>
+        [Route("pong/{text}")]
+        [HttpGet]
+        public async Task<String> Pong(String text)
+        {
+            var g = Client.GetGrain<IPingGrain>(Guid.Empty);
+            return await g.PongAsync(text);
+        }
+    
+
+
+    }
+}
---
+++ GrepoGrains/PingGrain.cs
@@ -0,0 +1,18 @@
+﻿using GrepoTools.Distributed.GrainInterfaces;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrepoTools.Distributed.Grains
+{
+    public class PingGrain : Grain, IPingGrain
+    {
+        async public Task<string> PongAsync(string value)
+        {
+            return value.ToUpper();
+        }
+    }
+}
