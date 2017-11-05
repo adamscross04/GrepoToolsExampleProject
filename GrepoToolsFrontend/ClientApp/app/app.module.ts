dddb639e103fec6389870931d623c2fa61bc3953
Adam Cross <tw2plus@outlook.com>
2017-11-05 21:13:17 +0000

    Adding angular material

--- GrepoToolsFrontend/ClientApp/app/app.module.ts
+++ GrepoToolsFrontend/ClientApp/app/app.module.ts
@@ -5,13 +5,20 @@ import { AppComponent } from './app.component';
 import { ViewsModule } from './views/views.module';
 import { MaterialModule } from './material/material.module';
 import { RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, ViewsModule, MaterialModule, RouterModule.forRoot([])
+    BrowserModule, 
+    ViewsModule, 
+    MaterialModule, 
+    RouterModule.forRoot([]), 
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- GrepoToolsFrontend/ClientApp/app/views/views.module.ts
+++ GrepoToolsFrontend/ClientApp/app/views/views.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    CommonModule,    RouterModule.forRoot([
+    CommonModule,    
+    RouterModule.forRoot([
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: MainComponent }
-    ])
+    ]), 
+    MaterialModule, 
+    FlexLayoutModule
   ],
   declarations: [MainComponent]
 })
